06a31644092c44085c3e54589aaf8e1d939d01fc
Fabián Lizama <96022469+FabianLizama@users.noreply.github.com>
2022-02-05 19:19:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import string
 SEPARATOR = (
         "####################################################################"
     )
+
+
 def menu():
     global SEPARATOR
     # Define the menu
     print(SEPARATOR)
     print("This program generate a secure and random password.")
     print("(Dont use whitespaces)")
-    
+
     print(SEPARATOR)
 
     # Ask for a character limit
@@ -43,7 +45,7 @@ def menu():
                     )
                     limit = int(character_limit)
                     break
-            else: 
+            else:
                 print("Please, enter a valid number.")
     else:
         print("Your password will be 12 characters long.")
@@ -86,12 +88,11 @@ def menu():
             # Separate the string in characters
             for i in range(len(excl_char)):
                 list_excl_char.append(excl_char[i])
-            
-            
+
             for i in list_excl_char:
                 if i in string.whitespace:
                     whitespaces += 1
-            
+
             if whitespaces == 0:
                 break
             else:
@@ -100,16 +101,17 @@ def menu():
         print(SEPARATOR)
     else:
         print(SEPARATOR)
-        list_excl_char =[]
+        list_excl_char = []
     print("Loading...")
     if limit > 10000:
         print("The input number is very long, it could take a long time.")
     elections = {
         "char_limit": limit,
         "spec_char": spec_char,
-        "excluded_chars": list_excl_char  
+        "excluded_chars": list_excl_char
     }
     return elections
+
 
 def pass_generator(elections):
     # Generate the password
@@ -127,18 +129,18 @@ def pass_generator(elections):
         low_chars.append(i)
     low_section = alpha_section - secrets.choice(low_chars)
     upp_section = alpha_section - low_section
-    
+
     special_section = (elections["char_limit"]-alpha_section)
-    
-    if elections["spec_char"] == True:
+
+    if elections["spec_char"] is True:
         symbol_chars = []
         for i in range(1, special_section):
             symbol_chars.append(i)
         symbols_section = special_section - secrets.choice(symbol_chars)
 
-    elif elections["spec_char"] == False:
+    elif elections["spec_char"] is False:
         symbols_section = 0
-    
+
     numeric_section = special_section - symbols_section
 
     # Determinate the password_characters
@@ -154,7 +156,7 @@ def pass_generator(elections):
     upp_alphabet = string.ascii_uppercase
     upp_alphabet_list = []
     for i in range(len(upp_alphabet)):
-        if upp_alphabet[i] not in elections["excluded_chars"]: 
+        if upp_alphabet[i] not in elections["excluded_chars"]:
             upp_alphabet_list.append(upp_alphabet[i])
 
     # Number characters setup
@@ -174,13 +176,13 @@ def pass_generator(elections):
     password_chars = []
     for i in range(low_section):
         password_chars.append(str(secrets.choice(low_alphabet_list)))
-    
+
     for i in range(upp_section):
         password_chars.append(secrets.choice(upp_alphabet_list))
-    
+
     for i in range(numeric_section):
         password_chars.append(secrets.choice(numbers_list))
-    
+
     for i in range(symbols_section):
         password_chars.append(secrets.choice(symbols_list))
 
@@ -192,6 +194,7 @@ def pass_generator(elections):
         password_chars.remove(actual_char)
     return password
 
+
 def pass_comprobation(password):
     # Check if the generated password is one of the most used worldwide
     passwords_list = open("insecure_passwords.txt", "r")
@@ -201,13 +204,14 @@ def pass_comprobation(password):
     else:
         return False
 
+
 def main():
     elections = menu()
     insecure_password = True
-    while insecure_password == True:
+    while insecure_password is True:
         password = pass_generator(elections)
         insecure_password = pass_comprobation(password)
-    
+
     global SEPARATOR
     print(SEPARATOR)
     print("Your unique generated password is:\n", password)
